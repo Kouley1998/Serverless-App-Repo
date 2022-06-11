4ff2711415af14206101b6200234e676bf40de68
Kouley Moussa <gebruiker@MacBook-Pro-van-Gebruiker.local>
2022-06-11 22:07:13 +0200

    added last

--- helloworld.js
+++ helloworld.js
@@ -1,15 +1,8 @@
 // code your `hello` function here!
 
-
-console.log(hello);
-
-
-
  function hello(){
-    const hello = require("./hello");
-    console.log("Hello World") 
+   return "Hello World"
 }
- hello()
 
- module.exports= hello
+ exports= hello;
 
