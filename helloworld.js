8a39c8f560ff007440ee74e00f49cd79888c82d6
Kouley Moussa <gebruiker@MacBook-Pro-van-Gebruiker.local>
2022-06-15 21:12:25 +0200

    uploaidng last tasks

--- helloworld.js
+++ helloworld.js
@@ -6,3 +6,4 @@ function hello() {
 } 
 
 exports.hello = hello;
+
--- late.js
+++ late.js
@@ -1,1 +1,18 @@
 // start coding your function here!
+
+//Create a function running_late(date)
+// in JavaScript that returns It is late! 
+//if the time is at or after 10:00 PM, and 
+//It is still early! if it is not.
+
+
+
+function running_late(){
+    const date = new Date()
+    console.log(date + "Its too early")
+  
+}
+
+running_late()
+
+module.exports = running_late
